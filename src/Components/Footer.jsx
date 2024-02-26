b7f8ba41d046c15258099a7db65b44ccfb72ee90
Alexander Swain <87860526+aswain03@users.noreply.github.com>
2024-02-26 14:09:24 +0000

    reverts prev changes made to package.json

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -114,7 +114,7 @@ const Footer = (props) => {
         )}
       </div>
       <p className="small" style={{ marginTop: 0, color: "white" }}>
-        Created by {name}
+        Created by {name}. Hosted on Azure.
       </p>
     </div>
   );
