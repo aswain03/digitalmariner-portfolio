de110e42fcf85e003b2242bbd74609fe3cf78ae4
Alexander Swain <87860526+aswain03@users.noreply.github.com>
2024-03-08 18:35:57 +0000

    Adds this website code to portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,35 +1,12 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
-
 import React, { useState, useEffect } from "react";
 
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/sunset-pixel-scenery-mountain.jpg";
 
 const imageAltText = "Pixel art of a sunset over a mountain range.";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
 const description =
   "I'm a Full stack engineer. Former Leading Hand in the Royal Navy. Studying Computer Science part time with the Open University. Data Engineer at Northcoders.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
 const skillsList = [
   "Python",
   "Cloud computing",
@@ -39,11 +16,6 @@ const skillsList = [
   "Game development",
 ];
 
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
 const detailOrQuote =
   "I am passionate about solving problems, incredibly curious and a strong believer in the power of the community, I am always looking to give back. Coding. Hiking. Cooking. Gardening. Modest. Moustache.";
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,34 +1,10 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
 
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/pixel_ship.jpg";
 
 const imageAltText = "Pixel art of a wooden ship on the sea.";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
+
 const projectList = [
   {
     title: "Backend fake news project 📰",
@@ -59,6 +35,11 @@ const projectList = [
     description:
       "First game I built using Unity. This was a great to learn the basics of game development and C#.",
     url: "https://digitalmariner.itch.io/snowrider-v1",
+  },
+  {
+    title: "This website! 💻",
+    description: "Constantly being updated and improved! Built using React and hosted on Azure.",
+    url: "https://github.com/aswain03/digitalmariner-portfolio",
   },
 ];
 
